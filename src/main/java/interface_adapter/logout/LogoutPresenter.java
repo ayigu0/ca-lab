c55f5405ac8416dc81377565d14c3beb4a8b747a
Sofia Borodaenko <soph.bor06@gmail.com>
2025-10-20 14:42:22 -0400

    fixed finally

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -35,10 +35,13 @@ public class LogoutPresenter implements LogoutOutputBoundary {
 
         final LoggedInState loggedInState = loggedInViewModel.getState();
         loggedInState.setUsername("");
+        loggedInState.setPassword("");
         this.loggedInViewModel.firePropertyChange();
+
 
         final LoginState loginState = loginViewModel.getState();
         loginState.setUsername(response.getUsername());
+        loginState.setPassword("");
         this.loginViewModel.firePropertyChange();
 
         // This code tells the View Manager to switch to the LoginView.
